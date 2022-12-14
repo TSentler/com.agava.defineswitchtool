d179b6710308f2e8730df7bf77dd1e22491c8219
TSentner <centner_dc@mail.ru>
2022-12-14 15:31:35 +0500

    Create DefineSymbolsData inside Assets folder

--- Editor/CustomDefines.cs
+++ Editor/CustomDefines.cs
@@ -7,10 +7,12 @@ namespace DefineSwitchTool.Editor
 {
     public class CustomDefines
     {
+        private const string _defineSwitchToolFolder = "DefineSwitchTool";
+
         public static readonly string DefineSymbolsDataPath =
-            "Packages/com.agava.defineswitchtool/Editor/"
+            "Assets/" + _defineSwitchToolFolder + "/Editor/"
             + typeof(DefineSymbolsData).Name + ".asset";
-        
+                
         private DefineSymbolsData _defineSymbolsData;
 
         public static string VkGamesName => "VK_GAMES";
@@ -55,6 +57,8 @@ namespace DefineSwitchTool.Editor
 
         private DefineSymbolsData CreateDefineSymbolsData()
         {
+            CreateFolder();
+            
             DefineSymbolsData asset =
                 ScriptableObject.CreateInstance<DefineSymbolsData>();
             string uniqAssetPath =
@@ -63,6 +67,23 @@ namespace DefineSwitchTool.Editor
             AssetDatabase.SaveAssets();
             AssetDatabase.Refresh();
             return AssetDatabase.LoadAssetAtPath<DefineSymbolsData>(DefineSymbolsDataPath);
+        }
+
+        private static void CreateFolder()
+        {
+            var path = "Assets";
+            var folder = _defineSwitchToolFolder;
+            if (AssetDatabase.IsValidFolder(path + "/" + folder) == false)
+            {
+                AssetDatabase.CreateFolder(path, folder);
+            }
+
+            path += "/" +folder;
+            folder = "Editor";
+            if (AssetDatabase.IsValidFolder(path + "/" + folder) == false)
+            {
+                AssetDatabase.CreateFolder(path, folder);
+            }
         }
 
         private DefineSymbolsData LoadDefineSymbolsData()
